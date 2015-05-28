911611b324a12e9cdbc36d887401517dff536d82
tsiolkas <tsiolkas@csd.uoc.gr>
2015-05-28 03:28:36 +0300

    measuring distance between 2 points

--- SAMakis_Project/web/JS/Categories_Functions.js
+++ SAMakis_Project/web/JS/Categories_Functions.js
@@ -20,10 +20,20 @@ $( document ).ready( function() {
                                                         "</div><div class=\"res_content\">"
                                                             +"Address: "+data.shops[i].address+"<br>"
                                                             +"Category: "+data.shops[i].category+"<br>"
-                                                            +"Views: "+data.shops[i].views+
+                                                            +"Views: "+data.shops[i].views+"<br>"
+                                                            +"d: "+calc(data.shops[i].lat,data.shops[i].lng)+
                                                     "</div></div>" );
             };
         } );
     } );
     
 } );
+
+function calc(lat,lng){
+    var lat1=35.3383767;
+    var lng1=25.1367471;
+    var dg=lng-lng1;
+    var d = 1.852 * 60 * Math.acos( Math.sin(lat1) * Math.sin(lat) + Math.cos(lat1) * Math.cos(lat) * Math.cos(dg));
+    
+    return Math.floor(d*1000);
+};
